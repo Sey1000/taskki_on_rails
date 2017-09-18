d955ef6651fbdb3a2b53e154fb4154b46da3cf95
Sey Cheon <22985121+Sey1000@users.noreply.github.com>
2017-09-18 12:25:54 +0200

    Complete setup for testing and successfully write first couple tests

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,20 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.user = current_user
+    @task.save
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :due)
+  end
 end
--- test/system/tasks_test.rb
+++ test/system/tasks_test.rb
@@ -3,6 +3,20 @@ require "application_system_test_case"
 class TasksTest < ApplicationSystemTestCase
   test "check h1" do
     visit '/'
+    # save_and_open_screenshot
     assert_selector "h1", text: "Tasks"
+    assert_selector ".task", count: Task.count
+  end
+
+  test "lets a signed in user create a new task" do
+    login_as users(:one)
+    visit '/tasks/new'
+
+    fill_in 'task_title', with: "Lunch with Sey"
+    fill_in 'task_due', with: "Sep 18"
+    click_on 'Create Task'
+
+    assert_equal root_path, page.current_path
+    assert_text "Lunch with Sey"
   end
 end
